74f44eb61afc1be66e1eedb5cc605327d75473ff
sakshi-nimje <115687946+sakshi-nimje@users.noreply.github.com>
2024-07-24 21:15:47 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,55 @@
+import java.io.Serializable;
+
+    public class Student implements Serializable {
+        private static final long serialVersionUID = 1L;
+        private String id;
+        private String name;
+        private int age;
+        private String course;
+
+        public Student(String id, String name, int age, String course) {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.course = course;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public String getCourse() {
+            return course;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public void setCourse(String course) {
+            this.course = course;
+        }
+
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "id='" + id + '\'' +
+                    ", name='" + name + '\'' +
+                    ", age=" + age +
+                    ", course='" + course + '\'' +
+                    '}';
+        }
+
+}
